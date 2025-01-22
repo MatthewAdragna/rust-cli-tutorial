109cb6eed0f510d58432ea5fe0d31335468eb84b
MatthewAdragna <matthewjadragna@gmail.com>
2025-01-22 01:07:40 -0500

    Some basic file opening functionality so far

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filepath = &args[2];
+    let grepped = &args[1];
+    println!("Filepath :{filepath:?}");
+
+    println!("ToGrep  :{grepped:?}");
+
+    let file_contents =
+        fs::read_to_string(filepath).expect("File could not be read. Program has panicked!");
+
+    println!("File {filepath} has been opened successfully.");
 }
