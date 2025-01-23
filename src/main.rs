b17111bd8c2c1f8379c83a08aebca13211f05f5e
MatthewAdragna <matthewjadragna@gmail.com>
2025-01-23 00:22:09 -0500

    Had it print out of the vector

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ use rct::mini_grep::run;
 use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    match run(&args) {
+    let index_vec = match run(&args) {
         Err(err) => panic!("Panic: {err:?}"),
-        _ => println!("Successfully completed mini_grep operation"),
+        Ok(vec) => vec,
     };
+
+    for index in index_vec {
+        println!("Found at {index}")
+    }
 }
