c46c0594957dbfbc95660bfc600285dc9f5e60a4
MatthewAdragna <matthewjadragna@gmail.com>
2025-01-25 18:59:54 -0500

    added a env just to have it done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use rct::mini_grep::run2_rawargs;
-
 use std::env;
 fn main() {
+    match env::var("funnyEnvironmentalVariable") {
+        Ok(a) => println!("Funny little Environmental variable is set to {a}"),
+        Err(a) => println!("Funny little Environmental Variable was not set :C  {a}"),
+    }
+
     let args: Vec<String> = env::args().collect();
     let index_vec = match run2_rawargs(&args) {
         Err(err) => panic!("Panic: {err:?}"),
